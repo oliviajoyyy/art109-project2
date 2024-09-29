5a09bbd5c30c8832641af097db6502c30fa9acd1
oliviajoyyy <127996216+oliviajoyyy@users.noreply.github.com>
2024-09-29 13:59:27 -0700

    css styling and code clean up

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ var counter = 0;
 // opacity screen
 var screen = document.querySelector(".screen");
 var currentOpacity = parseFloat(window.getComputedStyle(screen).getPropertyValue("opacity"));
+var minOp = 0.0;
+var maxOp = 0.8;
+var opIncrement = 0.1;
 
 // smiley face image, its width, and its increment value
 var smile = document.getElementById('smile');
@@ -17,14 +20,14 @@ const NEGATIVE = "negative"
 // an array of json objects of scenarios and options
 const prompts = [
     {
-        scenario: "You've been applying for jobs and have gotten a few interviews, but they fell through and you're not hearing back from most of the jobs you've applied to.",
-        optionA: "You decide to take time to improve your skills or learn new skills relevant to the career path you're interested in. You continute to apply for new job opportunies.",
+        scenario: "You've been applying for jobs, but you're not hearing back from most that you've applied to or the interviews you did have weren't successful.",
+        optionA: "You take time to learn new skills or improve your skills relevant to the career path you're interested in. You refine your resume, practice your interviewing skills, and continute to apply for new job opportunies.",
         optionA_status: POSITIVE,
         optionB: "You feel defeated, stay frusrated about it, and lose motivation to continue applying for jobs.",
         optionB_status: NEGATIVE
     },
     {
-        scenario: "All your friends are back in town and had you planned a fun day out on the weekend, but bad weather prevents you from carrying out your plans.",
+        scenario: "All your friends are back in town and you planned a fun day out for the weekend, but bad weather prevents you from carrying out your plans.",
         optionA: "Be disappointed and let it ruin the get-together you had planned.",
         optionA_status: NEGATIVE,
         optionB: "Organize plans for indoor activities instead and make the most of the time with your friends.",
@@ -39,16 +42,16 @@ const prompts = [
     },
     {
         scenario: "You're at work where a customer gets mad at you for how long they've waited in line.",
-        optionA: "Let their comments roll off your back. The customer was probably having a bad day themselves, and you're doing the best you can at this job.",
+        optionA: "Let their comments roll off your back. The customer was probably having a bad day, and you're doing the best you can at this job.",
         optionA_status: POSITIVE,
         optionB: "Dwell on the interaction and let it affect your mood for the rest of the day.",
         optionB_status: NEGATIVE
     },
     {
-        scenario: "You get stuck in traffic on your way to work due to a car accident that happened ahead, and it's going to cause you to be late.",
-        optionA: "Continue to stress out and worry about being late to work for the rest of the drive.",
+        scenario: "You get stuck in traffic on your way to work or school due to a car accident that happened ahead. There's no other route for you to go around it, and it's going to cause you to be late.",
+        optionA: "Continue to stress out and worry about being late for the rest of the drive.",
         optionA_status: NEGATIVE,
-        optionB: "De-stress about the traffic and being late by listening to music or a podcast. You might as well try to relax, you're stuck in the traffic either way.",
+        optionB: "De-stress about being late by listening to music or a podcast. You might as well try to relax, you're stuck in the traffic either way.",
         optionB_status: POSITIVE
     }
 ]
@@ -62,8 +65,6 @@ document.getElementById('optionB').innerHTML = prompts[counter].optionB;
 let buttons = document.querySelectorAll('.button');
 buttons.forEach((btn) => {
     btn.addEventListener('click', () => {
-
-        
 
         // get current opacity and width of img
         currentOpacity = parseFloat(window.getComputedStyle(screen).getPropertyValue("opacity"));
@@ -90,14 +91,10 @@ buttons.forEach((btn) => {
             }
         }
 
-        // increment counter and loop back if gone thru all of them
+        // increment counter and check if reached end of scenarios
         counter++;
         if (counter >= prompts.length) {
-            //counter = 0; // can instead do this with counter = counter+1 % prompts.length on line above
-            document.querySelector(".prompt-container").className = "prompt-container fade-out";
-            // document.querySelector("#prompt-container").style.visibility = "hidden";
-            document.querySelector("#life-quote").style.visibility = "visible";
-            document.querySelector("#life-quote").className = "fade-in";
+            endScenarios();
             return;
         }
 
@@ -114,10 +111,10 @@ buttons.forEach((btn) => {
 function positiveReaction() {
     smile.style.width = (smileWidth + wIncrement) + "px";
 
-    if (currentOpacity - 0.1 >= 0)
-        screen.style.opacity = currentOpacity - 0.1; // make image brighter
+    if (currentOpacity - opIncrement >= minOp)
+        screen.style.opacity = currentOpacity - opIncrement; // make background brighter
     else
-        screen.style.opacity = 0; // keep opacity from becoming a negative value
+        screen.style.opacity = minOp; // keep opacity from becoming a negative value
 }
 
 /**
@@ -125,16 +122,21 @@ function positiveReaction() {
  */
 function negativeReaction() {
     smile.style.width = (smileWidth - wIncrement) + "px";
-    
-    if (currentOpacity + 0.1 < 0.8)
-        screen.style.opacity = currentOpacity + 0.1; // make image darker
+
+    if (currentOpacity + opIncrement < maxOp)
+        screen.style.opacity = currentOpacity + opIncrement; // make background darker
     else
-        screen.style.opacity = 0.8; // keep opacity from going over 0.8
+        screen.style.opacity = maxOp; // keep opacity from going over 0.8
 }
 
 /**
  * When end of scenarios reached, show h1 title and hide prompt box
  */
-function end() {
-
+function endScenarios() {
+    if (currentOpacity >= 0.7) { // change text color to contrast better when background is darker
+        document.querySelector("#life-quote").style.color = "lightgrey";
+    }
+    document.querySelector(".prompt-container").className = "prompt-container fade-out";
+    document.querySelector("#life-quote").style.visibility = "visible";
+    document.querySelector("#life-quote").className = "fade-in";
 }
